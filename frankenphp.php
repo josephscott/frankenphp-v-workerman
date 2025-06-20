7203574e022cba4d7c9f551d78aff331dd7fc62c
Joseph Scott <joseph@josephscott.org>
2025-06-19 23:27:02 -0600

    Add basic frankenphp worker mode server script

---
+++ frankenphp.php
@@ -0,0 +1,23 @@
+<?php
+// From the https://frankenphp.dev/docs/worker/#custom-apps example
+ignore_user_abort( true );
+
+$handler = static function () {
+    echo "Hello World!\n";
+};
+
+$max_requests = (int) ( $_SERVER['MAX_REQUESTS'] ?? 0 );
+
+for (
+	$nb_requests = 0;
+	!$max_requests || $nb_requests < $max_requests;
+	++$nb_requests
+) {
+	$keep_running = frankenphp_handle_request( $handler );
+    gc_collect_cycles();
+
+	if (!$keep_running) {
+		break;
+	}
+}
+
