da6eb2187e5c97f7001d49082e00a4aa35173844
Joseph Scott <joseph@josephscott.org>
2025-06-20 20:25:32 -0600

    Add workerman server

---
+++ workerman.php
@@ -0,0 +1,27 @@
+<?php
+require __DIR__ . '/vendor/autoload.php';
+
+use Workerman\Worker;
+use Workerman\Protocols\Http\Request;
+use Workerman\Protocols\Http\Response;
+
+// Create HTTP server
+$http_worker = new Worker("http://0.0.0.0:80");
+
+// Set the number of processes (for benchmarking, you might want to adjust this)
+$http_worker->count = 4;
+
+// Handle HTTP requests
+$http_worker->onMessage = function($connection, Request $request) {
+    // Capture output from index.php
+    ob_start();
+    include __DIR__ . '/index.php';
+    $content = ob_get_clean();
+    
+    // Return HTTP response
+    $response = new Response(200, ['Content-Type' => 'text/html; charset=UTF-8'], $content);
+    $connection->send($response);
+};
+
+// Run the server
+Worker::runAll();
